2705e6cc1dc65a1e48e4ed8c512af9ee376395f7
shresthabi <62275895+shresthabi@users.noreply.github.com>
2020-03-20 14:56:01 +1100

    Add files via upload

---
+++ blink-task.ino
@@ -0,0 +1,111 @@
+
+int led1 = D7; 
+
+void setup() {
+
+  pinMode(led1, OUTPUT);
+
+}
+
+void loop() {
+  
+  //Morse code for alphabet B
+  
+  digitalWrite(led1, HIGH);
+ 
+  delay(3000);
+  
+  digitalWrite(led1, LOW);
+ 
+  delay(1000);
+  
+  digitalWrite(led1, HIGH);
+ 
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+ 
+  delay(1000);
+  
+  digitalWrite(led1, HIGH);
+ 
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+ 
+  delay(1000);
+
+  digitalWrite(led1, HIGH);
+
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+ 
+  delay(2000);
+  
+  //Morse code for alphabet I
+  
+  digitalWrite(led1, HIGH);
+ 
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+ 
+  delay(1000);
+
+  digitalWrite(led1, HIGH);
+
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+ 
+  delay(2000);
+  
+  //Morse code for alphabet N
+  
+  digitalWrite(led1, HIGH);
+ 
+  delay(3000);
+  
+  digitalWrite(led1, LOW);
+ 
+  delay(1000);
+  
+  digitalWrite(led1, HIGH);
+ 
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+ 
+  delay(2000);
+  
+  //Morse code for alphabet I
+  
+  digitalWrite(led1, HIGH);
+ 
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+ 
+  delay(1000);
+
+  digitalWrite(led1, HIGH);
+
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+ 
+  delay(2000);
+  
+  //Morse code for alphabet T
+  
+  digitalWrite(led1, HIGH);
+ 
+  delay(3000);
+  
+  digitalWrite(led1, LOW);
+ 
+  delay(10000);
+  
+  
+}
